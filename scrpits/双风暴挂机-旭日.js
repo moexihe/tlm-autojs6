dddd23eff7f66d6b7be01f998083d65ef28e88a4
moexihe <moexihe@outlook.com>
2026-07-16 23:02:36 +0800

    modified:   "scrpits/\345\217\214\351\243\216\346\232\264\346\214\202\346\234\272-\346\227\255\346\227\245.js"

--- scrpits/双风暴挂机-旭日.js
+++ scrpits/双风暴挂机-旭日.js
@@ -6,16 +6,16 @@ task();
 
 function task() {
     while (true) {
-        Utils.pressByPoint(Point.神速, Point.REF_WIDTH, Point.REF_HEIGHT);
+        Utils.pressByPoint(Point.神速,10, Point.REF_WIDTH, Point.REF_HEIGHT);
 
         for (var i = 0; i < 100; i++) {
-            Utils.pressByPoint(Point.攻击, Point.REF_WIDTH, Point.REF_HEIGHT);
+            Utils.pressByPoint(Point.攻击,10, Point.REF_WIDTH, Point.REF_HEIGHT);
             sleep(200);
 
             // 每 13 次攻击点一次其他位置 //技能释放次数是j/4
             if ((i + 1) % 12 === 0) {
                 [Point.旭日, Point.旭日, Point.旭日].forEach(p => {
-                    Utils.pressByPoint(p, Point.REF_WIDTH, Point.REF_HEIGHT);
+                    Utils.pressByPoint(p,10, Point.REF_WIDTH, Point.REF_HEIGHT);
                     sleep(50);
                 });
             }
