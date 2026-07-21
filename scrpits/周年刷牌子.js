d102e97da9129a1f4721836a3c741f08206e09f1
moexihe <moexihe@outlook.com>
2026-07-21 20:51:53 +0800

    modified:   "scrpits/\345\221\250\345\271\264\345\210\267\347\211\214\345\255\220.js"

--- scrpits/周年刷牌子.js
+++ scrpits/周年刷牌子.js
@@ -150,7 +150,7 @@ function detectScene() {
     }
 
     let skipText = U.ocrRegionCenter(P.全屏位置[0], P.全屏位置[1], P.REF_WIDTH, P.REF_HEIGHT) || [];
-    console.log("[战斗] 检查跳过按钮文字:", skipText);
+    console.log("[战斗] 检查文字:", skipText);
     let  Crystal = skipText.some(t => t && t.includes("水晶"));
     let  CrystalHP = U.findimg("/storage/emulated/0/脚本/scrpit/images/水晶血量.png");
     let time = skipText.some(t => t && t.includes("时间"));
