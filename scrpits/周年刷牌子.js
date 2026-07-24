d557e3ac01bffb8f4d8b7cafa73e0b0c947a1c28
moexihe <moexihe@outlook.com>
2026-07-25 06:53:21 +0800

    modified:   "scrpits/\345\221\250\345\271\264\345\210\267\347\211\214\345\255\220.js"

--- scrpits/周年刷牌子.js
+++ scrpits/周年刷牌子.js
@@ -71,9 +71,10 @@ function checkSpecialArena() {
     });
     while (rounds < 5) {
         sleep(500);
-        let skipText = U.ocrRegionCenter(P.跳过[0], P.跳过[1], P.REF_WIDTH, P.REF_HEIGHT) || [];
-        let skip = skipText.some(t => t && t.includes("跳过"));
-        let skip1 = skipText.some(t => t && t.includes("下一步"));
+        U.clickText("跳过")
+        // let skipText = U.clickText(P.跳过[0], P.跳过[1], P.REF_WIDTH, P.REF_HEIGHT) || [];
+        // let skip = skipText.some(t => t && t.includes("跳过"));
+        // let skip1 = skipText.some(t => t && t.includes("下一步"));
         if (skip || skip1) {
             U.clickByPoint(P.跳过, P.REF_WIDTH, P.REF_HEIGHT);
             break;
