04760b450903ea888d333f39ca8c0a60de517093
moexihe <moexihe@outlook.com>
2026-07-27 22:35:11 +0800

    new file:   "images/\347\264\240\346\235\220/\351\207\221\345\261\236/image.png" 	modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,25 +1,8 @@
 auto();
 const U = require("/storage/emulated/0/脚本/scrpit/utils/utils.js");
 const P = require("/storage/emulated/0/脚本/scrpit/constant/坐标.js");
-const ocr = require('paddleocr'); // 引入 PaddleOCR 库
-function isHomePage() {
-    console.log("检查是否在主页面");
-    sleep(1000);
-    let shop = U.isMainPage("/storage/emulated/0/脚本/scrpit/images/商店.png");
-          
-    // let qx = U.isMainPage("/storage/emulated/0/脚本/scrpit/images/情绪.png");
-    // console.log("检查主页面结果 => shop:", shop, "hp:", hp, "channel:", channel);
-    // console.log("最终结果:", shop && !hp && channel);
-    // return shop && !hp && channel;
-    console.log("检查主页面结果 => shop:", shop);
-}
-
-    // let skipText = U.ocrRegionCenter(P.全屏位置[0], P.全屏位置[1], P.REF_WIDTH, P.REF_HEIGHT) || [];
-    // console.log("[战斗] 检查跳过按钮文字:", skipText);
-    // if (skipText.some(t => t && t.includes("水晶")) || U.isMainPage("/storage/emulated/0/脚本/scrpit/images/水晶血量.png") &&
-    //     skipText.some(t => t && t.includes("时间"))) {
-    // }
-
-let test=ocr.paddle.detect()
-ocr.paddle.init()
-console.log(test)
+let img = captureScreen();
+const template = "/storage/emulated/0/脚本/scrpit/images/商店.png"
+let matchResult = U.findimg( template, { threshold: 0.80, max: 100 });
+console.log(matchResult);
+img.recyle;
