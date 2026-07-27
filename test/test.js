ca800e87d1c5e1070bc9622b288328f58ee0b620
moexihe <moexihe@outlook.com>
2026-07-27 22:44:07 +0800

    modified:   "images/\347\264\240\346\235\220/\345\205\275\345\223\201/0.png" 	modified:   "images/\347\264\240\346\235\220/\350\215\257\345\223\201/2.png" 	new file:   "images/\347\264\240\346\235\220/\350\215\257\345\223\201/3.png" 	modified:   "images/\347\264\240\346\235\220/\351\207\221\345\261\236/0.png" 	deleted:    "images/\347\264\240\346\235\220/\351\207\221\345\261\236/image.png" 	modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 auto();
 const U = require("/storage/emulated/0/脚本/scrpit/utils/utils.js");
 const P = require("/storage/emulated/0/脚本/scrpit/constant/坐标.js");
-let img = captureScreen();
-const template = "/storage/emulated/0/脚本/scrpit/images/商店.png"
+const template = "/storage/emulated/0/脚本/scrpit/images/素材/金属/image.png"
 let matchResult = U.findimg( template, { threshold: 0.80, max: 100 });
 console.log(matchResult);
-img.recyle;
