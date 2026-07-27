308a4c45fb438a60cf4395ca0d4e9439dd1ba122
moexihe <moexihe@outlook.com>
2026-07-27 22:46:30 +0800

    modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 auto();
 const U = require("/storage/emulated/0/脚本/scrpit/utils/utils.js");
 const P = require("/storage/emulated/0/脚本/scrpit/constant/坐标.js");
-const template = "/storage/emulated/0/脚本/scrpit/images/素材/金属/image.png"
+const template = "/storage/emulated/0/脚本/scrpit/images/素材/金属/0.png"
 let matchResult = U.findimg( template, { threshold: 0.80, max: 100 });
 console.log(matchResult);
