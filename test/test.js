b5508ec02257f76d895df83d55e7f8ea1365b945
moexihe <moexihe@outlook.com>
2026-07-30 08:03:09 +0800

    modified:   test/test.js

--- test/test.js
+++ test/test.js
@@ -2,5 +2,15 @@ auto();
 const U = require("/storage/emulated/0/脚本/scrpit/utils/utils.js");
 const P = require("/storage/emulated/0/脚本/scrpit/constant/坐标.js");
 const template = "/storage/emulated/0/脚本/scrpit/images/素材/金属/0.png"
-let matchResult = U.findimg( template, { threshold: 0.80, max: 100 });
-console.log(matchResult);
+
+    threads.start(function(){
+        while(true){
+            sleep(3000);
+            console.log("清理内存中")
+            console.clear();
+        }
+    })
+
+    while (true){
+        console.log("11"); 
+        sleep(1000)}
